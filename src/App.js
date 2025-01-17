a21fa2759fb5787e7bfbc40c284c59ae04e698e4
McKay Seamons <mseamons5@hotmail.com>
2025-01-16 21:52:00 -0800

    mobile fixes

--- src/App.js
+++ src/App.js
@@ -102,25 +102,27 @@ const handleInputChange = (event) => {
 
   return (
     <div className={`relative flex flex-col items-center justify-center h-screen ${theme.background}`}>
-      <h1 className={`text-3xl font-bold mb-6 ${theme.text}`}>Sweet Spot Calculator</h1>
+<h1 className={`text-3xl font-bold mt-4 mb-4 ${theme.text} sm:mt-12`}>
+  Sweet Spot Calculator
+</h1>
+
       
       {/* Night/Day Mode Toggle */}
-      <div className="absolute top-4 right-4 flex items-center">
-        <span className={`${theme.text} mr-2`}>🌞</span>
-        <label className="relative inline-flex items-center cursor-pointer">
-          <input
-            type="checkbox"
-            className="sr-only peer"
-            checked={darkMode}
-            onChange={toggleDarkMode}
-          />
-          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-        </label>
-        <span className={`${theme.text} ml-2`}>🌙</span>
-      </div>
+      <div className="absolute top-4 right-4 flex items-center hidden sm:flex">
+  <span className={`${theme.text} mr-2`}>🌞</span>
+  <label className="relative inline-flex items-center cursor-pointer">
+    <input
+      type="checkbox"
+      className="sr-only peer"
+      checked={darkMode}
+      onChange={toggleDarkMode}
+    />
+    <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+  </label>
+  <span className={`${theme.text} ml-2`}>🌙</span>
+</div>
 
-      <div className="mb-6">
-        <label htmlFor="skillInput" className={`text-lg font-medium ${theme.text}`}>
+      <div className="mb-6 flex sm:w-full sm:justify-center">        <label htmlFor="skillInput" className={`text-lg font-medium ${theme.text}`}>
           Enter Your Skill Level:
         </label>
         <input
